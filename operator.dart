14085bff5c13219e84da977fca636241e55a0a24
reaz24 <reazuddin.dev.bd@gmail.com>
2024-08-28 08:21:38 -0700

    Add files via upload

---
+++ operator.dart
@@ -0,0 +1,50 @@
+main(){
+  // dart operatror
+  int a = 110;
+  int b = 10;
+  int sum = a + b;
+  double div = a / b;
+  int min = a - b;
+  int  mul = a * b;
+  int res = a % b;
+
+  print(sum);
+  print(div);
+  print(min);
+  print(mul);
+  print(res);
+
+
+  // dart unary operator
+
+  int x = 10;
+  print(++x);
+  print(++x);
+  print(++x);
+  print(++x);
+  print(++x);
+  print(++x);
+  print(++x);
+
+  int y = 10;
+  print(y--);
+  print(y--);
+  print(y--);
+  print(y--);
+  print(y--);
+  print(y--);
+  print(y--);
+  print(y--);
+
+
+  // dart constant
+
+  const year = 2024;
+
+  print(year);
+
+    final age = 20;
+  print(age);
+
+
+}
